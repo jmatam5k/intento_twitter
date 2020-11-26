8c1afe345a0736eb6b6694c8b4369b4a6576c602
Jennifer Matamala <katana@MacBook-Pro-de-Jennifer.local>
2020-11-25 21:23:46 -0300

    se crea modelo user

--- app/controllers/tweeeets_controller.rb
+++ app/controllers/tweeeets_controller.rb
@@ -29,7 +29,7 @@ class TweeeetsController < ApplicationController
 
     respond_to do |format|
       if @tweeeet.save
-        format.html { redirect_to @tweeeet, notice: 'Tweeeet was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Tweeeet was successfully created.' }
         format.json { render :show, status: :created, location: @tweeeet }
       else
         format.html { render :new }
